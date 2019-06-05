7c36a1cb8c4541b64f42b2a3289a1c70bc60ab82
d-luu <david@davidluu.info>
2019-06-04 22:18:41 -0500

    added ca cert

--- files/run.sh
+++ files/run.sh
@@ -9,12 +9,13 @@ HAPROXY_CFG_VALIDATE="/usr/local/sbin/haproxy -f ${HAPROXY_CFG} -c"
 RSYSLOG_PID="/var/run/rsyslogd.pid"
 RSYSLOG_RUN="/usr/sbin/rsyslogd"
 
+CA_CERT="${HAPROXY_FOLDER}/root_ca.crt"
 TLS_CERT="${HAPROXY_FOLDER}/node.crt"
 TLS_KEY="${HAPROXY_FOLDER}/node.key"
 TLS_CHAIN="${HAPROXY_FOLDER}/node.pem"
 
 echo "Creating cert/key file..."
-/bin/cat "${TLS_CERT}" "${TLS_KEY}" > "${TLS_CHAIN}"
+/bin/cat "${TLS_CERT}" "${CA_CERT}" "${TLS_KEY}" > "${TLS_CHAIN}"
 echo "Cert/key file created."
 
 echo "Starting rsyslogd..."
@@ -29,9 +30,9 @@ echo "HAProxy started."
 
 echo "Watching for changes to HAProxy config or cert/key..."
 
-inotifywait -m "${HAPROXY_CFG}" "${TLS_CERT}" "${TLS_KEY}" -e create,delete,modify,attrib |
+inotifywait -m "${HAPROXY_CFG}" "${TLS_CERT}" "${CA_CERT}" "${TLS_KEY}" -e create,delete,modify,attrib |
   while read path; do
-    /bin/cat "${TLS_CERT}" "${TLS_KEY}" > "${TLS_CHAIN}"
+    /bin/cat "${TLS_CERT}" "${CA_CERT}" "${TLS_KEY}" > "${TLS_CHAIN}"
 
     if [ -f "${HAPROXY_PID}" ]; then
       echo "Restart HAProxy due to file changes in folder..."
